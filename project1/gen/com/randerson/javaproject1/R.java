a34104fd363cf0c949f826ec26e80364f859872d
Rueben Anderson <rueand713@fullsail.edu>
2013-05-08 15:37:58 -0500

    LinearLayout update
    Added a function for adding views to the parent layout to shorten the
    amount of code needed to write.

--- project1/gen/com/randerson/javaproject1/R.java
+++ project1/gen/com/randerson/javaproject1/R.java
@@ -24,15 +24,42 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f080000;
+    }
+    public static final class integer {
+        public static final int barbarian=0x7f040008;
+        public static final int common=0x7f040009;
+        public static final int corpse=0x7f040005;
+        public static final int evolved=0x7f04000b;
+        public static final int lich=0x7f040003;
+        public static final int lycan=0x7f040002;
+        public static final int mutant=0x7f040006;
+        public static final int orc=0x7f040007;
+        public static final int skeleton=0x7f040004;
+        public static final int vampire=0x7f040001;
+        public static final int veteran=0x7f04000a;
+        public static final int zombie=0x7f040000;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int barbarian=0x7f05000b;
+        public static final int common=0x7f05000c;
+        public static final int corpse=0x7f050008;
+        public static final int description=0x7f05000f;
+        public static final int evolved=0x7f05000e;
+        public static final int lich=0x7f050006;
+        public static final int lycan=0x7f050005;
+        public static final int mutant=0x7f050009;
+        public static final int namecreature=0x7f050002;
+        public static final int orc=0x7f05000a;
+        public static final int skeleton=0x7f050007;
+        public static final int vampire=0x7f050004;
+        public static final int veteran=0x7f05000d;
+        public static final int zombie=0x7f050003;
     }
     public static final class style {
         /** 
@@ -56,10 +83,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- project1/src/com/randerson/javaproject1/MainActivity.java
+++ project1/src/com/randerson/javaproject1/MainActivity.java
@@ -3,13 +3,91 @@ package com.randerson.javaproject1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	// method for creating and adding a view with a textEdit and button to the parent view
+	// the string passed in is a reference to a value in the strings.xml resource file
+	// the layout object should be the parent layout that the completed view should be added to
+	public void addCharacterView(String name, LinearLayout layout)
+	{
+		// create a vertical sub-parent layout to house its own views
+		// this view will then be added (along with its children) to the parent view
+		LinearLayout vLayout = new LinearLayout(this);
+		vLayout.setOrientation(LinearLayout.VERTICAL);
+		LinearLayout.LayoutParams vLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		vLayout.setLayoutParams(vLayoutParams);
+		
+		// create the TextView object for describing the current monster
+		// using the passed in name string and the description string.xml reference
+		// also for referencing and separation of the UI elements
+		TextView description = new TextView(this);
+		description.setText(name + " " + getString(R.string.description));
+		
+		// create the editText and button objects to add them to the objectLayout view
+		EditText monsterName = new EditText(this);
+		monsterName.setHint(getString(R.string.namecreature));
+		Button monsterSelect = new Button(this);
+		monsterSelect.setText(name);
+		
+		// create monster class buttons to add to the view
+		// the monster class is a reference to a string in the strings.xml resource file
+		Button monsterClassCommon = new Button(this);
+		monsterClassCommon.setText(getString(R.string.common));
+		Button monsterClassVeteran = new Button(this);
+		monsterClassVeteran.setText(getString(R.string.veteran));
+		Button monsterClassEvolved = new Button(this);
+		monsterClassEvolved.setText(getString(R.string.evolved));
+		
+		
+		// create a horizontal layout to house the monster class buttons
+		// and a horizontal layout to house the editText and monster select button
+		// these views will then be added to the vertical layout view
+		
+		// layout for the monster buttons
+		LinearLayout hLayoutA = new LinearLayout(this);
+		hLayoutA.setOrientation(LinearLayout.HORIZONTAL);
+		
+		// layout for the monster selection
+		LinearLayout hLayoutB = new LinearLayout(this);
+		hLayoutB.setOrientation(LinearLayout.HORIZONTAL);
+		
+		LinearLayout.LayoutParams hLayoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+		hLayoutA.setLayoutParams(hLayoutParams);
+		hLayoutB.setLayoutParams(hLayoutParams);
+		
+		// add the sub views to their container view
+		hLayoutA.addView(monsterClassCommon);
+		hLayoutA.addView(monsterClassVeteran);
+		hLayoutA.addView(monsterClassEvolved);
+		
+		hLayoutB.addView(monsterName);
+		hLayoutB.addView(monsterSelect);
+		
+		// add the horizontal views to their parent vertical view
+		vLayout.addView(hLayoutA);
+		vLayout.addView(hLayoutB);
+		
+		// add the fully created view set to the main view
+		layout.addView(vLayout);
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// set the initial values
+		int totalPoints = 500;
+		int[] monsterElements = {
+				R.string.corpse, R.string.barbarian, R.string.mutant, 
+				R.string.skeleton, R.string.orc, R.string.zombie,
+				R.string.lycan, R.string.vampire, R.string.lich
+				};
+		
 		
 		// create the main layout. This view will be the 
 		// top layer view for the application and will hold all
@@ -19,7 +97,12 @@ public class MainActivity extends Activity {
 		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 		mainLayout.setLayoutParams(layoutParams);
 		
-		
+		// iterate through the monsterElements and create a linear layout 
+		// for each of the monster classes
+		for(int i = 0; i < monsterElements.length; i++)
+		{
+			addCharacterView(getString(monsterElements[i]), mainLayout);
+		}
 	}
 
 	@Override
