6fd18abb0eed040333a1b9ee9d2e65a0015fcdee
Rueben Anderson <rueand713@fullsail.edu>
2013-05-16 10:56:42 -0500

    Create JSONFactory class
    The JSONFactory class will use its methods to create and read JSON
    objects

---
+++ project2/src/handlingJSON/JSONFactory.java
@@ -0,0 +1,18 @@
+package handlingJSON;
+
+import org.json.JSONObject;
+
+public class JSONFactory {
+
+	public static JSONObject createJSONObject()
+	{
+		JSONObject thisJSONObject = new JSONObject();
+		
+		for (JSONData json : JSONData.values())
+		{
+			
+		}
+		
+		return thisJSONObject;
+	}
+}
