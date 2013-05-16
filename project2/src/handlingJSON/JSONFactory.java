cbd4f04dbfcd5cde471aac51de65b042d9851a38
Rueben Anderson <rueand713@fullsail.edu>
2013-05-16 11:21:02 -0500

    createJSONObject method added

--- project2/src/handlingJSON/JSONFactory.java
+++ project2/src/handlingJSON/JSONFactory.java
@@ -1,18 +1,50 @@
 package handlingJSON;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JSONFactory {
 
+	// method for creating a JSON object with the JSONData enum data
 	public static JSONObject createJSONObject()
 	{
-		JSONObject thisJSONObject = new JSONObject();
+		// create the master JSON object
+		JSONObject queryObject = new JSONObject();
 		
-		for (JSONData json : JSONData.values())
-		{
+		// create the parent JSON object
+		JSONObject parentObject = new JSONObject();
+		
+		try {
+			for (JSONData json : JSONData.values())
+			{
+				// create the child JSON object
+				JSONObject childObject = new JSONObject();
+				
+				// add the enum data to the child JSON object
+				childObject.put("developer", json.getDeveloper());
+				childObject.put("platform", json.getPlatform());
+				childObject.put("title", json.getTitle());
+				childObject.put("release", json.getRelease());
+				
+				// add the child JSON object to its parent
+				parentObject.put(json.name().toString(), childObject);
+			}
 			
+			// add the parent JSON object to the master queryObject
+			queryObject.put("query", parentObject);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
-		return thisJSONObject;
+		// return the new JSON object
+		return queryObject;
+	}
+	
+	// method for reading the JSON object returned from the createJSONObject method
+	public static void readJSONObject()
+	{
+		
 	}
 }
