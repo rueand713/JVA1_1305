7376e33434d37353e65628207b74c19894e3402e
Rueben Anderson <rueand713@fullsail.edu>
2013-05-16 09:04:35 -0500

    Finalized the createUI class
    Added the methods for creating an EditText, TextView, and Button. These
    methods take in 3 parameters and return the created object.

--- project2/src/interfaceElements/CreateUI.java
+++ project2/src/interfaceElements/CreateUI.java
@@ -1,7 +1,10 @@
 package interfaceElements;
 
 import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CreateUI {
 
@@ -44,7 +47,55 @@ public class CreateUI {
 			thisLayout.setLayoutParams(layoutParams);
 		}
 		
+		// return the object
 		return thisLayout;
 	}
 	
+	// method for creating and returing an editText object
+	public static EditText createEditText(Context context, String hint, int id)
+	{
+		// creates a new editText object
+		EditText thisEditText = new EditText(context);
+		
+		// set the editText object hint
+		thisEditText.setHint(hint);
+		
+		// set the editText object identifier
+		thisEditText.setId(id);
+		
+		// return the object
+		return thisEditText;
+	}
+	
+	// method for creating and returning a textView object
+	public static TextView createTextView(Context context, String text, int id)
+	{
+		// creates a new textView object
+		TextView thisTextView = new TextView(context);
+		
+		// sets the text for the textView object
+		thisTextView.setText(text);
+		
+		// sets the textView object identifier
+		thisTextView.setId(id);
+		
+		// return the object
+		return thisTextView;
+	}
+	
+	// method for creating and returning a button object
+	public static Button createButton(Context context, String text, int id)
+	{
+		// creates a new button object
+		Button thisButton = new Button(context);
+		
+		// sets the button text
+		thisButton.setText(text);
+
+		// sets the button object identifier
+		thisButton.setId(id);
+		
+		// return the object
+		return thisButton;
+	}
 }
