7cb5c1232bb9ac1b5eeccaba2a60e99b867cf71f
Rueben Anderson <rueand713@fullsail.edu>
2013-05-23 17:10:40 -0500

    Updated app now makes proper requests

--- project3/src/com/randerson/java1project3/MainActivity.java
+++ project3/src/com/randerson/java1project3/MainActivity.java
@@ -1,11 +1,23 @@
 package com.randerson.java1project3;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import networkingMethods.connectionManager;
+import handleJSON.JSONFactory;
 import interfaceMethods.CreateUI;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
@@ -14,23 +26,35 @@ public class MainActivity extends Activity {
 	// creates a new instance of the CreateUI class
 	CreateUI UIFactory = new CreateUI(this);
 	
-	// creates the main, sub, detail, and detailViews layouts with the UIFactory instance
+	// set up the main, sub, detail, and detailViews layouts
 	LinearLayout mainLayout;
 	LinearLayout subLayout;
 	LinearLayout detailLayout;
 	LinearLayout leftDetailView;
 	LinearLayout rightDetailView;
 	
-	// creates the text views for the weather detail layout
+	// setup the views for weather detail layout
 	TextView currentConditionValue;
 	TextView tempValue;
 	TextView humidityValue;
 	TextView windSpeedValue;
 	TextView windDirValue;
 	
+	// setup the radio group
+	RadioGroup radios;
+	
+	// Boolean for the connection status
+	Boolean connected;
+	
+	// URL for the get request
+	URL weatherURL;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// checks if there is a network connection
+		connected = connectionManager.getConnectionStatus(this);
 		
 		// integers for representing the various view ids
 		int textId = 133;
@@ -39,9 +63,9 @@ public class MainActivity extends Activity {
 		
 		// api request string parts
 		// the strings will be concatenated with the selected location and passed as the request
-		String apiKey = getString(R.string.apikey);
-		String restStringA = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=";
-		String restStringB = "&format=json&num_of_days=5&key=" + apiKey;
+		final String apiKey = getString(R.string.apikey);
+		final String restStringA = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=";
+		final String restStringB = "&format=json&num_of_days=5&key=" + apiKey;
 		
 		// strings for the various app views
 		String instructions = getString(R.string.instructions);
@@ -77,10 +101,44 @@ public class MainActivity extends Activity {
 		TextView windDir = UIFactory.createTextView("Wind Direction: ", 0);
 		
 		// create the radiogroup with the UIFactory instance
-		RadioGroup radios = UIFactory.createRadioGroup(locales, radioId);
+		radios = UIFactory.createRadioGroup(locales, radioId);
 		
 		// creates the button with the UIFactory instance
 		Button sendBtn = UIFactory.createButton(btnText, buttonId);
+		
+		// set up the on click for the send button
+		sendBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				// get the id of the currently selected radio button
+				int selectedId = radios.getCheckedRadioButtonId();
+				
+				// create an instance of the radio button that is currently selected
+				RadioButton rBtn = (RadioButton) findViewById(selectedId);
+				
+				// retrieve the selected button text value from the instance
+				String selectedValue = rBtn.getText().toString();
+				
+				// check to make sure there was an active network
+				if (connected)
+				{
+					try {
+						// create a URL object from the api strings
+						weatherURL = new URL(restStringA + selectedValue + restStringB);
+						
+						doRequest req = new doRequest();
+						
+						req.execute(weatherURL);
+						
+					} catch (MalformedURLException e) {
+						Log.e("URL ERROR", "Malformed URL");
+					}
+					
+				}
+			}
+		});
 		
 		// add the child views to the sublayout
 		subLayout.addView(instText);
@@ -120,6 +178,60 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	// android class thread for asynchronous requests
+	private class doRequest extends AsyncTask<URL, Void, String>
+	{
+		
+		@Override
+		protected String doInBackground(URL...urls)
+		{
+			// create a empty response string
+			String response = "";
+			
+			// loop for making url request with URLs 
+			for (URL url:urls)
+			{
+				// response gets set for each request made (in this instance just one)
+				response = connectionManager.makeStringRequest(url);
+			}
+			
+			// return the response text
+			return response;
+		}
+		
+		@Override
+		protected void onPostExecute(String result)
+		{
+			// log the result text
+			Log.i("RESPONSE", result);
+			
+			// create JSON objects from the result string
+			// that object is then querried for the particular key and the string is returned and set
+			String temp = JSONFactory.readJSONObject(result, "temp_F");
+			String humidity = JSONFactory.readJSONObject(result, "humidity");
+			String condition = "";
+			String windSpeed = JSONFactory.readJSONObject(result, "windspeedMiles");
+			String windDirection = JSONFactory.readJSONObject(result, "winddir16Point");
+			
+			// create a new separate JSON object
+			JSONObject cc = JSONFactory.returnJSONObject(result);
+			
+			try {
+				// retrieve the deep nested weather condition string
+				condition = cc.getJSONObject("data").getJSONArray("current_condition").getJSONObject(0).getJSONArray("weatherDesc").getJSONObject(0).getString("value");
+			} catch (JSONException e) {
+				Log.e("JSON ERROR", "JSON Exception");
+			}
+			
+			// set the detail view data
+			currentConditionValue.setText(condition);
+			humidityValue.setText(humidity);
+			tempValue.setText(temp);
+			windSpeedValue.setText(windSpeed);
+			windDirValue.setText(windDirection);
+		}
 	}
 
 }
